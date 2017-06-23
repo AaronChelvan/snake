db31941b3d72b935e0d876a759161640fa851ec6
AaronChelvan <aaron.chelvan@outlook.com>
2017-06-23 20:54:14 +1000

    Added movement methods

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-//Canvas setup. These are 2 global variables.
-var canvas = document.getElementById("playingField");
-var context = canvas.getContext("2d");
-
 //Coordinate object constructor
-// 0 <= x <= 31; 0 <= y <= 31; (32 x 32 grid)
+//0 <= x <= 31; 0 <= y <= 31; (32 x 32 grid)
 function Coordinate(x,y) {
     this.x = x;
     this.y = y;
+    this.getX = function () {
+        return this.x;
+    };
+    this.getY = function () {
+        return this.y;
+    }
 }
 
 //Snake object constructor
@@ -21,10 +23,53 @@ function Snake() {
         var startingLocation = new Coordinate(15, 15+i);
         this.location.push(startingLocation);
     }
+
+    //The coordinates of the head of the snake (i.e. this.location[0])
+    this.head = new Coordinate(this.location[0].getX(), this.location[0].getY());
+
+    //Movement methods
+    this.moveUp = function() {
+        this.direction = "up";
+        this.location.pop(); //Remove the end of the tail.
+        //Add the new location to the beginning of the location array.
+        var newHeadLocation = new Coordinate(this.head.getX(), this.head.getY()+1);
+        this.location.unshift(newHeadLocation);
+        //Update the head
+        this.head = new Coordinate(this.head.getX(), this.head.getY()+1);
+    };
+    this.moveDown = function() {
+        this.direction = "down";
+        this.location.pop(); //Remove the end of the tail.
+        //Add the new location to the beginning of the location array.
+        var newHeadLocation = new Coordinate(this.head.getX(), this.head.getY()+1);
+        this.location.unshift(newHeadLocation);
+        //Update the head
+        this.head = new Coordinate(this.head.getX(), this.head.getY()+1);
+    }
+    this.moveLeft= function() {
+        this.direction = "left";
+        this.location.pop(); //Remove the end of the tail.
+        //Add the new location to the beginning of the location array.
+        var newHeadLocation = new Coordinate(this.head.getX(), this.head.getY()+1);
+        this.location.unshift(newHeadLocation);
+        //Update the head
+        this.head = new Coordinate(this.head.getX(), this.head.getY()+1);
+    }
+    this.moveRight = function() {
+        this.direction = "right";
+        this.location.pop(); //Remove the end of the tail.
+        //Add the new location to the beginning of the location array.
+        var newHeadLocation = new Coordinate(this.head.getX(), this.head.getY()+1);
+        this.location.unshift(newHeadLocation);
+        //Update the head
+        this.head = new Coordinate(this.head.getX(), this.head.getY()+1);
+    }
 }
 
 //Given a Coordinate object, colour the corresponding square on the canvas.
 function fillCoords(coordinate) {
+    var canvas = document.getElementById("playingField");
+    var context = canvas.getContext("2d");
     context.fillRect(coordinate.x*10,310-coordinate.y*10,10,10);
 }
 
