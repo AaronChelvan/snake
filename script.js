bc68e57ba4cec62672c61d64f2614ab2553b994e
AaronChelvan <aaron.chelvan@outlook.com>
2017-06-23 23:10:25 +1000

    Improved movement
    - Snake moves one space per time interval
    - Removed ability to move backwards

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 function Coordinate(x,y) {
     this.x = x;
     this.y = y;
-    this.getX = function () {
+    this.getX = function() {
         return this.x;
     };
-    this.getY = function () {
+    this.getY = function() {
         return this.y;
     }
 }
@@ -14,6 +14,9 @@ function Coordinate(x,y) {
 //Snake object constructor
 function Snake() {
     this.direction = "up"; //The direction the snake is initially facing.
+    this.getDirection = function() {
+        return this.direction;
+    };
     this.location = [];//An array of (x,y) coordinates representing the squares the snake occupies.
                 //The first item is the head.
                 //The last item is the end of the tail.
@@ -73,8 +76,8 @@ function fillCoords(coordinate) {
     context.fillRect(coordinate.getX()*10,310-coordinate.getY()*10,10,10);
 }
 
-//Given a Snake object, print the entire snake onto the canvas
-function printSnake(playerSnake) {
+//Print the entire snake onto the canvas
+function printSnake() {
     for (var i = 0; i < playerSnake.location.length; i++) {
         fillCoords(playerSnake.location[i]);
     }
@@ -87,39 +90,87 @@ function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+//Update the game state
+function updateGame() {
+    switch (lastKeyPress) {
+        case null: //If a non-arrow key was pressed, keep moving in the direction the snake is currently facing
+            if (playerSnake.getDirection() == "left") {
+                playerSnake.moveLeft();
+            } else if (playerSnake.getDirection() == "up") {
+                playerSnake.moveUp();
+            } else if (playerSnake.getDirection() == "right") {
+                playerSnake.moveRight();
+            } else if (playerSnake.getDirection() == "down") {
+                playerSnake.moveDown();
+            }
+            break;
+        case "left": //Left arrow
+            if (playerSnake.getDirection() == "right") { //Can't move left if facing right
+                playerSnake.moveRight(); //Keep moving right
+                break;
+            }
+            playerSnake.moveLeft();
+            break;
+        case "up": //Up arrow
+            if (playerSnake.getDirection() == "down") { //Can't move up if facing down
+                playerSnake.moveDown(); //Keep moving down
+                break;
+            }
+            playerSnake.moveUp();
+            break;
+        case "right": //Right arrow;
+            if (playerSnake.getDirection() == "left") { //Can't move right if facing left
+                playerSnake.moveLeft(); //Keep moving left
+                break;
+            }
+            playerSnake.moveRight();
+            break;
+        case "down": //Down arrow;
+            if (playerSnake.getDirection() == "up") { //Can't move down if facing up
+                playerSnake.moveUp();//Keep moving up
+                break;
+            }
+            playerSnake.moveDown();
+            break;
+    }
+    clearCanvas(); //Clear the canvas
+    printSnake(playerSnake); //Print the snake onto the new updated location
+    lastKeyPress = null; //Clear the last key press
+}
+
 //Create the player and print it
-var playerSnake = new Snake();
+var playerSnake = new Snake(); //Global variable
 printSnake(playerSnake);
 
-//Start listening for key presses
+//Global variable containing a string describing the last key press. "up", "left", etc.
+//Contains null if a non-arrow key.
+var lastKeyPress = null;
+
+//Listen for key presses
 document.onkeydown = function checkKeyPressed(key) {
     switch (key.keyCode) {
-        case 37:
+        case 37: //Left arrow
             console.log("Key press recorded: left arrow");
-            clearCanvas();
-            playerSnake.moveLeft();
-            printSnake(playerSnake);
-            return "left";
-        case 38:
+            lastKeyPress = "left";
+            break;
+        case 38: //Up arrow
             console.log("Key press recorded: up arrow");
-            clearCanvas();
-            playerSnake.moveUp();
-            printSnake(playerSnake);
-            return "up";
-        case 39:
+            lastKeyPress = "up";
+            break;
+        case 39: //Right arrow
             console.log("Key press recorded: right arrow");
-            clearCanvas();
-            playerSnake.moveRight();
-            printSnake(playerSnake);
-            return "right";
-        case 40:
+            lastKeyPress = "right";
+            break;
+        case 40: //Down arrow
             console.log("Key press recorded: down arrow");
-            clearCanvas();
-            playerSnake.moveDown();
-            printSnake(playerSnake);
-            return "down";
+            lastKeyPress = "down";
+            break;
         default:
-            console.log("Key press recorded: not an arrow key"); //debug
-            return null;
+            console.log("Key press recorded: not an arrow key");
+            lastKeyPress = null;
+            break;
     }
+    return;
 }
+
+setInterval(updateGame, 200); //Refresh the game state at a specified time interval
